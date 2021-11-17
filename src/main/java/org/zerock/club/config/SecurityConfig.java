741e84d4bd1929099d0b47cb4129a66185cd9ee1
dohyun1994 <hiyaya94@gmail.com>
2021-11-17 16:43:37 +0900

    spring security

---
+++ src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package org.zerock.club.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@Log4j2
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        
+        http.authorizeRequests()
+                .antMatchers("/sample/all").permitAll()
+                .antMatchers("/sample/member").hasRole("USER");
+            http.formLogin();       // 인가/인증에 문제시 로그인화면
+            http.csrf().disable();  // 주석 처리하면 로그아웃 창 뜨고,  주석 처리 하지 않으면, /sample/member에서 로그인 후 localhost:8081/logout으로 이동하면 로그아웃 창 뜨지않고 바로 로그아웃됨.
+            http.logout();
+    }
+
+}
